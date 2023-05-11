d9dad73be75e2b1a6762ab6eeed8cd2d820afa12
dokidokiabr <125663291+dokidokiabr@users.noreply.github.com>
2023-05-11 08:24:10 -0300

    mainview added

---
+++ src/view/MainView.java
@@ -0,0 +1,285 @@
+package view;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author joeziojr
+ */
+public class MainView extends javax.swing.JFrame {
+
+    /**
+     * Creates new form MainView
+     */
+    public MainView() {
+        initComponents();
+        setLocationRelativeTo(this);
+        this.setExtendedState(MAXIMIZED_BOTH);
+    }
+    
+    //getters and setters
+    public JLabel getLblMinutes() {
+        return lblMinutes;
+    }
+
+    public void setLblMinutes(JLabel lblMinutes) {
+        this.lblMinutes = lblMinutes;
+    }
+
+    public JLabel getLblSeconds() {
+        return lblSeconds;
+    }
+
+    public void setLblSeconds(JLabel lblSeconds) {
+        this.lblSeconds = lblSeconds;
+    }
+    
+    //buttons getters
+    public JButton getBtnLogin() {
+        return btnLogin;
+    }
+
+    public JButton getBtnSettings() {
+        return btnSettings;
+    }
+
+    public JButton getBtnStart() {
+        return btnStart;
+    }
+
+    public JButton getBtnStop() {
+        return btnStop;
+    }
+    
+    public JButton getBtnReset() {
+        return btnReset;
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        jPanel1 = new javax.swing.JPanel();
+        btnSettings = new javax.swing.JButton();
+        btnLogin = new javax.swing.JButton();
+        btnStart = new javax.swing.JButton();
+        jPanel2 = new javax.swing.JPanel();
+        lblMinutes = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        lblSeconds = new javax.swing.JLabel();
+        btnStop = new javax.swing.JButton();
+        btnReset = new javax.swing.JButton();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Pocotoro");
+
+        jPanel1.setBackground(new java.awt.Color(235, 81, 96));
+
+        btnSettings.setBackground(new java.awt.Color(128, 143, 133));
+        btnSettings.setForeground(new java.awt.Color(255, 255, 255));
+        btnSettings.setText("Configuração");
+        btnSettings.setBorderPainted(false);
+        btnSettings.setFocusPainted(false);
+
+        btnLogin.setBackground(new java.awt.Color(128, 143, 133));
+        btnLogin.setForeground(new java.awt.Color(255, 255, 255));
+        btnLogin.setText("Login");
+        btnLogin.setBorderPainted(false);
+        btnLogin.setFocusPainted(false);
+
+        btnStart.setBackground(new java.awt.Color(220, 224, 217));
+        btnStart.setFont(new java.awt.Font("Liberation Sans", 0, 24)); // NOI18N
+        btnStart.setForeground(new java.awt.Color(0, 0, 0));
+        btnStart.setText("Iniciar");
+        btnStart.setFocusPainted(false);
+        btnStart.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btnStartMouseClicked(evt);
+            }
+        });
+        btnStart.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnStartActionPerformed(evt);
+            }
+        });
+
+        jPanel2.setBackground(new java.awt.Color(238, 109, 122));
+
+        lblMinutes.setFont(new java.awt.Font("Liberation Sans", 0, 170)); // NOI18N
+        lblMinutes.setForeground(new java.awt.Color(220, 224, 217));
+        lblMinutes.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblMinutes.setText("25");
+
+        jLabel2.setFont(new java.awt.Font("Liberation Sans", 0, 170)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(220, 224, 217));
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel2.setText(":");
+
+        lblSeconds.setFont(new java.awt.Font("Liberation Sans", 0, 170)); // NOI18N
+        lblSeconds.setForeground(new java.awt.Color(220, 224, 217));
+        lblSeconds.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        lblSeconds.setText("00");
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(lblMinutes)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 33, Short.MAX_VALUE)
+                .addComponent(jLabel2)
+                .addGap(33, 33, 33)
+                .addComponent(lblSeconds)
+                .addGap(20, 20, 20))
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                .addContainerGap(58, Short.MAX_VALUE)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                        .addComponent(lblMinutes)
+                        .addGap(13, 13, 13))
+                    .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jLabel2)
+                        .addComponent(lblSeconds)))
+                .addGap(58, 58, 58))
+        );
+
+        btnStop.setBackground(new java.awt.Color(220, 224, 217));
+        btnStop.setFont(new java.awt.Font("Liberation Sans", 0, 24)); // NOI18N
+        btnStop.setForeground(new java.awt.Color(0, 0, 0));
+        btnStop.setText("Parar");
+        btnStop.setFocusPainted(false);
+
+        btnReset.setBackground(new java.awt.Color(220, 224, 217));
+        btnReset.setFont(new java.awt.Font("Liberation Sans", 0, 24)); // NOI18N
+        btnReset.setForeground(new java.awt.Color(0, 0, 0));
+        btnReset.setText("Reset");
+        btnReset.setFocusPainted(false);
+        btnReset.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btnResetMouseClicked(evt);
+            }
+        });
+        btnReset.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnResetActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(164, 164, 164)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(btnLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 89, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(btnSettings))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 337, Short.MAX_VALUE)))
+                .addContainerGap())
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(315, 315, 315)
+                .addComponent(btnStart, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(btnStop, javax.swing.GroupLayout.PREFERRED_SIZE, 102, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(375, 375, 375)
+                .addComponent(btnReset, javax.swing.GroupLayout.PREFERRED_SIZE, 110, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnSettings)
+                    .addComponent(btnLogin))
+                .addGap(144, 144, 144)
+                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(66, 66, 66)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnStart, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(btnStop, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(btnReset, javax.swing.GroupLayout.PREFERRED_SIZE, 28, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(90, Short.MAX_VALUE))
+        );
+
+        getContentPane().add(jPanel1, java.awt.BorderLayout.CENTER);
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void btnStartMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnStartMouseClicked
+    }//GEN-LAST:event_btnStartMouseClicked
+
+    private void btnStartActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnStartActionPerformed
+    }//GEN-LAST:event_btnStartActionPerformed
+
+    private void btnResetMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnResetMouseClicked
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnResetMouseClicked
+
+    private void btnResetActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnResetActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btnResetActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MainView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MainView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MainView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MainView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MainView().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnLogin;
+    private javax.swing.JButton btnReset;
+    private javax.swing.JButton btnSettings;
+    private javax.swing.JButton btnStart;
+    private javax.swing.JButton btnStop;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JLabel lblMinutes;
+    private javax.swing.JLabel lblSeconds;
+    // End of variables declaration//GEN-END:variables
+}
