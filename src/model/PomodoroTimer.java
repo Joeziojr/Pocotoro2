3f645a948f75975f8d5cf9fb0acc49847786ce5e
dokidokiabr <125663291+dokidokiabr@users.noreply.github.com>
2023-05-11 08:16:51 -0300

    Update PomodoroTimer.java

--- src/model/PomodoroTimer.java
+++ src/model/PomodoroTimer.java
@@ -3,10 +3,13 @@ package model;
 public class PomodoroTimer {
     //atributos
     private int seconds;
+    private final int idPomodoro;
+    private int duration;
     
     //construtor
-    public PomodoroTimer(int duration) {
+    public PomodoroTimer(int duration, int idPomodoro) {
         this.seconds = duration * 60;
+        this.idPomodoro = idPomodoro;
     }
     
     //getters e setters
@@ -17,5 +20,20 @@ public class PomodoroTimer {
     public void setSeconds(int seconds) {
         this.seconds = seconds;
     }
+
+    public int getIdPomodoro() {
+        return idPomodoro;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+    
+    
+    
     
 }
